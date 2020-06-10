96ea1303c0a0ad6da6b0e72552bb3dd3260b43d1
EmmaLeu <manuela.leu@uipath.com>
2020-06-10 23:40:19 +0300

    Initial commit. Setup simple frontend and backend apps

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/EmmaLeu/ChatApp
+
+go 1.14
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func setupRoutes() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, "Simple server")
+	})
+}
+
+func main() {
+
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
