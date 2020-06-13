199a97464dcd9ba8445c6e3ed8e06cb298921eb6
EmmaLeu <manuela.leu@uipath.com>
2020-06-13 12:07:00 +0300

    Create WebSocket connection and establish simple communication

--- backend/main.go
+++ backend/main.go
@@ -2,19 +2,63 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+// Connection upgrader
+var upgrader = websocket.Upgrader{
+
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+// Define reader which will listen to all
+// messages being sent to out WebSocket endpoint
+func reader(conn *websocket.Conn) {
+	for {
+
+		messageType, p, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+// Define our WebSocket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	//  Upgrade this connection to a WebSocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reader(ws)
+}
+
 func setupRoutes() {
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
 		fmt.Fprintf(w, "Simple server")
 	})
+	// Map our `/ws` endpoint to the `serveWs` function
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
