5ad13570c0c436933f9552998e70f930b8ea5bf9
EmmaLeu <manuela.leu@uipath.com>
2020-06-14 11:29:17 +0300

    Add node-sass module; Add Header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss"
+
+const Header = () => (
+    <div className = "Header">
+        <h2>Realtime Chat App</h2>
+    </div>
+);
+
+export default Header;
